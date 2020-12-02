60d6a2210ad69f66d1ee5419fbfe72f449e04449
Netto Nucci <nettonucci@gmail.com>
2020-12-02 08:39:39 -0300

    4 - Diferenças do ReactJS
    Algumas informações sobre React Native

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Helo World!</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
